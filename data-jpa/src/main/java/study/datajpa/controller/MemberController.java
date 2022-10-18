7c94449a04c570a6ac3d38047f309fe7449c1210
donghyoya <backjui@kookmin.ac.kr>
2022-10-18 20:40:07 +0900

    paging, sort

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -15,15 +19,30 @@ public class MemberController {
 
     private final MemberRepository memberRepository;
 
+    @GetMapping("/members/remove")
+    public void removeData(){
+        memberRepository.deleteAll();
+    }
+
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id")Long id){
         Member member = memberRepository.findById(id).get();
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size=5,sort="id") Pageable pageable){
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> map = page.map(MemberDto::new);
+        return map;
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("testUser",30));
+//        memberRepository.save(new Member("testUser",30));
+        for(int i = 0; i<100; i++){
+            memberRepository.save(new Member("user"+i,i));
+        }
     }
 
 }
--- data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
+++ data-jpa/src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,10 +1,10 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 public class MemberDto {
-
     private Long id;
     private String username;
     private String teamName;
@@ -13,5 +13,11 @@ public class MemberDto {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
+    }
+
+    public MemberDto(Member member){
+        this.id = member.getId();
+        this.username = member.getUsername();
+        this.teamName = member.getTeam().getName();
     }
 }
