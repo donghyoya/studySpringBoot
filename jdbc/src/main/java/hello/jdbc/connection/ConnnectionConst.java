04606c873b5f6821d2f7c2611a83beb23b36b84e
donghyoya <backjui@kookmin.ac.kr>
2023-04-04 22:12:05 +0900

    jdbcUtilTest

---
+++ jdbc/src/main/java/hello/jdbc/connection/ConnnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public abstract class ConnnectionConst {
+    public static final String URL = "jdbc:mysql://localhost:3306/jdbc_study?useSSL=false&allowPublicKeyRetrieval=true&useUnicode=true&serverTimezone=Asia/Seoul";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "0814";
+}
