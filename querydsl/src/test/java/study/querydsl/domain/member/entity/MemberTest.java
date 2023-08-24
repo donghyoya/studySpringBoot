b665132c94e90dfc0e207d71183471db728a2478
kdw <dongwooya123@gmail.com>
2023-08-24 11:30:24 +0900

    update select, sort, paging, group, join

--- querydsl/src/test/java/study/querydsl/domain/member/entity/MemberTest.java
+++ querydsl/src/test/java/study/querydsl/domain/member/entity/MemberTest.java
@@ -1,10 +1,15 @@
 package study.querydsl.domain.member.entity;
 
+import com.querydsl.core.QueryFactory;
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import study.querydsl.domain.team.entity.QTeam;
 import study.querydsl.domain.team.entity.Team;
 import study.querydsl.domain.member.entity.Member;
 
@@ -20,6 +25,19 @@ public class MemberTest {
 
     @Autowired
     EntityManager em;
+
+    JPAQueryFactory jpaQueryFactory;
+
+    QMember qMember;
+    QTeam qTeam;
+
+    @BeforeEach
+    public void befor(){
+        jpaQueryFactory = new JPAQueryFactory(em);
+
+        qMember = new QMember("qMember");
+        qTeam = new QTeam("qTeam");
+    }
 
     @Test
     @Rollback(value = true)
@@ -81,4 +99,183 @@ public class MemberTest {
         assertThat(member1.getUsername()).isEqualTo("member1");
     }
 
+    @Test
+    public void querydslSearch(){
+        QMember m = new QMember("m");
+        Member findMember = jpaQueryFactory
+                .selectFrom(m)
+                .where(m.username.eq("member1")
+                        .and(m.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void querydslSearch2(){
+        QMember m = new QMember("m");
+        Member findMember = jpaQueryFactory
+                .selectFrom(m)
+                .where(m.username.eq("member1")
+                        .and(m.age.between(9, 15)))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void querydslSearch3(){
+        //and 대신 , 으로 가능
+        QMember m = new QMember("m");
+        Member findMember = jpaQueryFactory
+                .selectFrom(m)
+                .where(m.username.eq("member1"),
+                        m.age.between(9, 15))
+                .fetchOne();
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void querydslSearchList(){
+        QMember m = new QMember("m");
+
+        //쿼리 동작 1
+        List<Member> member1 = jpaQueryFactory
+                .selectFrom(m)
+                .fetch();
+
+        System.out.println("member = " + member1);
+
+//        Member member1 = jpaQueryFactory
+//                .selectFrom(m)
+//                .fetchOne();
+//        System.out.println("member1 = " + member1);
+        //쿼리 동작 2
+        Member member2 = jpaQueryFactory
+                .selectFrom(m)
+                .fetchFirst();
+        System.out.println("member2 = " + member2);
+
+        //쿼리 동작 3
+        QueryResults<Member> memberResults = jpaQueryFactory
+                .selectFrom(m)
+                .fetchResults();
+
+        System.out.println("memberResults = " + memberResults);
+        //쿼리 동작 4
+        System.out.println("memberResults.getResults() = " + memberResults.getResults());
+        System.out.println("memberResults.getTotal() = " + memberResults.getTotal());
+
+
+        long count = jpaQueryFactory
+                .selectFrom(m)
+                .fetchCount();
+        System.out.println("count = " + count);
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2 에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void querydslSort(){
+        QMember m = new QMember("m");
+
+        em.persist(new Member(null,100));
+        em.persist(new Member("member10",100));
+        em.persist(new Member("member11",100));
+
+        List<Member> members = jpaQueryFactory
+                .selectFrom(m)
+                .where(m.age.eq(100))
+                .orderBy(m.age.desc(),
+                        m.username.asc().nullsLast())
+                .fetch();
+        for(Member member2 : members){
+            System.out.println("member = " + member2);
+        }
+    }
+
+    @Test
+    public void querydslPaging(){
+        QMember m = new QMember("m");
+        List<Member> members = jpaQueryFactory
+                .selectFrom(m)
+                .orderBy(m.username.desc())
+                .offset(2)  //2번째 부터 시작한다
+                .limit(4)   //4번째까지만 가져오겠다
+                .fetch();
+
+        for(Member member2: members){
+            System.out.println("member = " + member2);
+        }
+    }
+
+    @Test
+    public void querydslPaging2(){
+        QMember m = new QMember("m");
+
+        QueryResults<Member> results = jpaQueryFactory
+                .selectFrom(m)
+                .orderBy(m.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+//        assertThat(results.getTotal()).isEqualTo(4);
+//        assertThat(results.getLimit()).isEqualTo(2);
+//        assertThat(results.getOffset()).isEqualTo(1);
+//        assertThat(results.getResults().size()).isEqualTo(2);
+
+    }
+
+    @Test
+    public void querydslGroupby(){
+        List<Tuple> result = jpaQueryFactory
+                .select(
+                        qMember.count(),
+                        qMember.age.sum(),
+                        qMember.age.max(),
+                        qMember.age.avg(),
+                        qMember.age.min()
+                )
+                .from(qMember)
+                .fetch();
+
+        Tuple get0 = result.get(0);
+        assertThat(get0.get(qMember.count())).isEqualTo(5);
+        assertThat(get0.get(qMember.age.sum())).isEqualTo(125);
+    }
+
+    @Test
+    public void querydslGroupby2(){
+        List<Tuple> tuples = jpaQueryFactory
+                .select(qTeam.name, qMember.age.avg())
+                .from(qMember)
+                .join(qMember.team, qTeam)
+                .groupBy(qTeam.name)
+                .fetch();
+
+        Tuple teamA = tuples.get(0);
+        Tuple teamB = tuples.get(1);
+
+        System.out.println("teamC = " + teamA);
+        System.out.println("teamD = " + teamB);
+
+    }
+
+    @Test
+    public void join(){
+        List<Member> members = jpaQueryFactory
+                .selectFrom(qMember)
+                .join(qMember.team, qTeam)
+                .where(qTeam.name.eq("teamC"))
+                .fetch();
+
+        for(Member member : members){
+            System.out.println("member = " + member);
+        }
+    }
 }
